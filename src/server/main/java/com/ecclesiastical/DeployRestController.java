b56ee88b756da61b369b17978854344d1539959c
Stewart Dunlop <radixdeath@gmail.com>
2017-08-25 16:33:30 +0100

    Got some of the backend restful services working - need to test the json response and get the spa part working

--- src/server/main/java/com/ecclesiastical/DeployRestController.java
+++ src/server/main/java/com/ecclesiastical/DeployRestController.java
@@ -1,12 +1,39 @@
 package com.ecclesiastical;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 public class DeployRestController {
 
-    @RequestMapping("/eccdeployer/helloworld")
+    @RequestMapping("/example")
     public String greet() {
         return "Hello from the other side!!!";
     }
+
+    @RequestMapping("/example/getBackendData")
+    public ResponseEntity getBackendData() {
+        ExampleData data1 = new ExampleData();
+        data1.setBackendItem1("Backend Item 1_1");
+        data1.setBackendItem1("Backend Item 1_2");
+        data1.setBackendItem1("Backend Item 1_3");
+        ExampleData data2 = new ExampleData();
+        data1.setBackendItem1("Backend Item 2_1");
+        data1.setBackendItem1("Backend Item 2_2");
+        data1.setBackendItem1("Backend Item 2_3");
+
+        List<ExampleData> data = new ArrayList<ExampleData>();
+        data.add(data1);
+        data.add(data2);
+        return new ResponseEntity(data, HttpStatus.OK);
+        //return "something";
+    }
+
+
+
 }
---
+++ src/server/main/java/com/ecclesiastical/ExampleData.java
@@ -0,0 +1,34 @@
+package com.ecclesiastical;
+
+public class ExampleData {
+
+    private String backendItem1;
+
+    private String backendItem2;
+
+    private String backendItem3;
+
+    public String getBackendItem1() {
+        return backendItem1;
+    }
+
+    public void setBackendItem1(String backendItem1) {
+        this.backendItem1 = backendItem1;
+    }
+
+    public String getBackendItem2() {
+        return backendItem2;
+    }
+
+    public void setBackendItem2(String backendItem2) {
+        this.backendItem2 = backendItem2;
+    }
+
+    public String getBackendItem3() {
+        return backendItem3;
+    }
+
+    public void setBackendItem3(String backendItem3) {
+        this.backendItem3 = backendItem3;
+    }
+}
