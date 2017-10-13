ab98abe9b822bd5b08924df3c2d6aeb43a99ec57
Stewart Dunlop <stewartdunlop@Stewarts-MacBook-Pro.local>
2017-10-13 13:14:35 +0100

    Added postcode validation linked to Find Address button enablement

--- src/frontend/src/modules/hhd-quote/property-details-section/property-details-section.component.ts
+++ src/frontend/src/modules/hhd-quote/property-details-section/property-details-section.component.ts
@@ -1,26 +1,27 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 import { PropertyDetailsModel} from "./property-details.model";
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {logging} from "selenium-webdriver";
-import {logger} from "codelyzer/util/logger";
+import {FormBuilder, FormControl, FormGroup, NG_VALIDATORS, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'ecc-property-details-section',
   templateUrl: './property-details-section.component.html',
+  //providers: [ { provide: NG_VALIDATORS, useValue: validatePostcode, multi: true } ],
   styleUrls: ['./property-details-section.component.less']
 })
+
 export class PropertyDetailsSectionComponent implements OnInit {
 
   phoneNumber: string = '0345 604 6483';
 
   propertyDetailsFormGroup: FormGroup;
   houseNumberFormCtrl = new FormControl("", Validators.required);
-  postcodeFormCtrl = new FormControl("", Validators.required);
+  postcodeFormCtrl = new FormControl("", [ Validators.required, postcodeValidator ]);
 
   propertyDetailsModel: PropertyDetailsModel;
-  //@Input() propertyDetailsModel: PropertyDetailsModel;
-  //@Output() propertDetailsModelChange = new EventEmitter<string>()
+
+  addressesFound = [];
+  addressSelected: string;
 
   constructor(fb: FormBuilder) {
     this.propertyDetailsModel = new PropertyDetailsModel();
@@ -34,12 +35,14 @@ export class PropertyDetailsSectionComponent implements OnInit {
     this.houseNumberFormCtrl.valueChanges
       .map((value) => {
         // do some processing here
-        console.log('value is '+JSON.stringify(value));
+        //console.log('value is '+JSON.stringify(value));
         return value;
       })
       .subscribe((value) => {
         this.propertyDetailsModel.ecc_houseNumber = value;
       });
+
+
     this.postcodeFormCtrl.valueChanges
       .map((value) => {
         // do some processing here
@@ -53,4 +56,31 @@ export class PropertyDetailsSectionComponent implements OnInit {
       });
   }
 
+  findAddresses() {
+    if (!this.postcodeFormCtrl.invalid) {
+      this.addressesFound = buildAddresses();
+      console.log('find addresses');
+    }
+  }
+
+
 }
+
+  function buildAddresses(): string[] {
+    let adds = [];
+    for (let i=0; i<36; i++) {
+      adds[i] = i+' Footshill Close, Hanham, Bristol';
+    }
+    return adds;
+  }
+
+  function postcodeValidator(formControl: FormControl) {
+    const EMAIL_REGXP = /^([A-Z][A-Z0-9]{0,3}\s?[0-9][A-Z0-9]{2})$/i;
+    let pcode = formControl.value.replace(/\s/g,'');
+    return EMAIL_REGXP.test(pcode) ? null : {
+      validatePostcode: {
+        valid: false
+      }
+    };
+  }
+
